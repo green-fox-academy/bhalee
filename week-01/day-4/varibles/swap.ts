78801ffbabc94f67dd64fc9d20a53d2500e4bcfc
Balazs Kover <koverbalazs@gmail.com>
2020-11-19 16:17:43 +0100

    swap.ts added

---
+++ week-01/day-4/varibles/swap.ts
@@ -0,0 +1,12 @@
+'use strict';
+
+let numberToSwap1: number = 123;
+let numberToSwap2: number = 526;
+
+console.log (numberToSwap1);
+console.log (numberToSwap2);
+
+[numberToSwap1, numberToSwap2] = [numberToSwap2, numberToSwap1]
+
+console.log (numberToSwap1);
+console.log (numberToSwap2);
