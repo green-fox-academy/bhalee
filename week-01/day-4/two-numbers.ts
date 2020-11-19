6fe3bbb01b4c05e269a1747dcca63f73d4dfbb6b
Balazs Kover <koverbalazs@gmail.com>
2020-11-19 14:49:21 +0100

    two-numbers.ts added

--- week-01/day-4/two-numbers.ts
+++ week-01/day-4/two-numbers.ts
@@ -1,9 +1,10 @@
 'use strict';
 let i:number = 13;
 let j:number = 22;
+let k:number = ( j / i);
 
 console.log ( i + j );
 console.log ( j - i );
 console.log ( j * i );
-console.log ( j / i );
+console.log ( j / i, ' kerekitve tizedesig: ', k.toFixed(1) );
 console.log ( j % i );
