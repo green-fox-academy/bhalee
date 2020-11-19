eeeef451ab3c3e5f2cb4771c0a798c559c6409a1
Balazs Kover <koverbalazs@gmail.com>
2020-11-19 14:33:36 +0100

    Start Types Exercises

---
+++ week-01/day-4/introduce-yourself.ts
@@ -0,0 +1,7 @@
+'use strict';
+let myName = 'Kover Balazs';
+let myAge = 29;
+let myHeight = 1.7;
+console.log ( myName );
+console.log ( myAge );
+console.log ( myHeight );
---
+++ week-01/day-4/two-numbers.ts
@@ -0,0 +1,9 @@
+'use strict';
+let i:number = 13;
+let j:number = 22;
+
+console.log ( i + j );
+console.log ( j - i );
+console.log ( j * i );
+console.log ( j / i );
+console.log ( j % i );
