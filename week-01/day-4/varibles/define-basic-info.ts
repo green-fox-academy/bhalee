6521c442e15a5e1adcd5a7f74190e5a71fb1cdbe
Balazs Kover <koverbalazs@gmail.com>
2020-11-19 16:57:07 +0100

    define-basic-info.ts done

---
+++ week-01/day-4/varibles/define-basic-info.ts
@@ -0,0 +1,20 @@
+'use strict';
+
+let myName: string = 'Kover Balazs';
+let myAge: number = 29;
+let myHeight: number = 1.70;
+let imMarried: boolean = false;
+
+console.log ('My name is', myName);
+console.log ('I\'m',myAge,'years old' );
+console.log ('I\'m',myHeight,'meter height' );
+
+if (imMarried === false) {
+
+    console.log ('I\'m not married');
+
+} else if (imMarried === true) {
+
+    console.log ('I\'m married');
+
+}
