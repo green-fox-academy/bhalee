f2aa90220b39db8751e9b92ee3dadb42ae21adb2
Balazs Kover <koverbalazs@gmail.com>
2020-11-21 10:52:03 +0100

    dra-pyramid.ts made without reapet

--- week-01/day-4/loops/draw-diamond.ts
+++ week-01/day-4/loops/draw-diamond.ts
@@ -1,18 +1,8 @@
 'use strict';
 export{}
-let lineCount: number = 41; // minimum: 7 and odd number looks like more diamond :)
-let star: any = '\*';
-let space: any = ' ';
+let lineCount: number = 7;
 
-for (let i: number = 1, j: number = lineCount - 4, k: number = - 3, l: number = lineCount; i <= lineCount; i++, j--, k++, l--) {
-if (i < lineCount / 2){
-    console.log ((space.repeat(j))+(star.repeat(i))+(star.repeat(i-1)));
-} else 
-{
-    console.log ((space.repeat(k))+(star.repeat(l)+(star.repeat(l-1))));
-}
 
-}
 
 // Write a program that draws a
 // diamond like this:
@@ -26,3 +16,19 @@ if (i < lineCount / 2){
 //    *
 //
 // The diamond should have as many lines as lineCount is
+/*
+'use strict';
+export{}
+let lineCount: number = 41; // minimum: 7 and odd number looks like more diamond :)
+let star: any = '\*';
+let space: any = ' ';
+
+for (let i: number = 1, j: number = lineCount - 4, k: number = - 3, l: number = lineCount; i <= lineCount; i++, j--, k++, l--) {
+if (i < lineCount / 2){
+    console.log ((space.repeat(j))+(star.repeat(i))+(star.repeat(i-1)));
+} else 
+{
+    console.log ((space.repeat(k))+(star.repeat(l)+(star.repeat(l-1))));
+}
+
+} */
--- week-01/day-4/loops/draw-pyramid.ts
+++ week-01/day-4/loops/draw-pyramid.ts
@@ -1,14 +1,22 @@
 'use strict';
 export{}
 let lineCount: number = 4;
-let star: any = '\*';
-let space: any = ' ';
+let star:string = '*'
 
-for (let i: number = 1, j: number = lineCount + 1; i <= lineCount; i++, j--) {
+for (let i:number = 1, j:number = lineCount - i; i <= lineCount; i++, j-- ){
 
-    console.log ((space.repeat(j))+(star.repeat(i))+(star.repeat(i-1)));
+        let space: string = '';
+
+for (let j:number = lineCount - i; j > 0; j--) {
+    space += ' ';
+    
+}
+
+console.log(space+star);
+star += '**';
 
 }
+
 
 // Write a program that draws a
 // pyramid like this:
@@ -19,3 +27,16 @@ for (let i: number = 1, j: number = lineCount + 1; i <= lineCount; i++, j--) {
 // *******
 //
 // The pyramid should have as many lines as lineCount is
+/*
+'use strict';
+export{}
+let lineCount: number = 4;
+let star: any = '\*';
+let space: any = ' ';
+
+for (let i: number = 1, j: number = lineCount + 1; i <= lineCount; i++, j--) {
+
+    console.log ((space.repeat(j))+(star.repeat(i))+(star.repeat(i-1)));
+
+} 
+*/
