d7831c8978b434900945ff62715b936d47105797
Balazs Kover <koverbalazs@gmail.com>
2020-11-23 14:46:16 +0100

    array exercices started

---
+++ week-02/day-1/sum-elements.ts
@@ -0,0 +1,11 @@
+// - Create a variable named `numbers`
+//   with the following content: `[54, 23, 66, 12]`
+// - Print the sum of the second and the third element
+
+let numbers: number[] = [54, 23, 66, 12];
+let j:number = numbers[1];
+let i:number = numbers[2];
+
+console.log( j + i )
+ 
+
