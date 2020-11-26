6cc77b2d56324f9d8ff18407da6a5e6195f0da35
Balazs Kover <koverbalazs@gmail.com>
2020-11-26 16:41:00 +0100

    data structures started again

---
+++ week-02/day-2/data-structures/list-introduction-2.ts
@@ -0,0 +1,23 @@
+let listA: string[] = ['Apple', 'Avocado', 'Blueberries', 'Durian', 'Lychee'];
+let listB: string[] = [...listA];
+
+if (listA.includes('Durian')) {
+  console.log('yes');
+} else {
+  console.log('nooo');
+}
+listB.splice(3, 1);
+
+listA.splice(3, 0, 'Kiwifruit');
+
+console.log(`listA = ${listA.length} listB = ${listB.length}`);
+if (listA.length > listB.length) {
+  console.log('listA is bigger');
+} else {
+  console.log('listB is bigger');
+}
+
+console.log(listA.indexOf('Avocado'));
+console.log(listB.indexOf('Durian'));
+listB.push('Passion Fruit', 'Pomelo');
+console.log(listA[3]);
