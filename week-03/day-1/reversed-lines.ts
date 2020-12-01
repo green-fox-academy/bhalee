e45b0c5441eabb2984f0a162ef959422114274be
Balazs Kover <koverbalazs@gmail.com>
2020-12-01 16:52:31 +0100

    reversed-lines.ts fixed and reversed-order.ts done

--- week-03/day-1/reversed-lines.ts
+++ week-03/day-1/reversed-lines.ts
@@ -2,14 +2,11 @@
 export {};
 // Create a method that decrypts reversed-lines.txt
 
-('use strict');
-export {};
-// Create a method that decrypts duplicated-chars.txt
 const fs = require('fs');
 function fileName(x): void {
   try {
     let fileContent: string = fs.readFileSync(x, 'utf-8').toString();
-    let decryptsText = fileContent.split('').reverse().join('');
+    let decryptsText = fileContent.split('').reverse().join('').split('\r').reverse().join('');
 
     fs.writeFileSync('reversed-lines-fixed.txt', decryptsText);
     console.log(decryptsText);
---
+++ week-03/day-1/reversed-order.ts
@@ -0,0 +1,17 @@
+// Create a method that decrypts reversed-order.txt
+('use strict');
+export {};
+const fs = require('fs');
+function fileName(x): void {
+  try {
+    let fileContent: string = fs.readFileSync(x, 'utf-8').toString();
+    let decryptsText = fileContent.split('\n\r').reverse().join('');
+
+    fs.writeFileSync('reversed-order-fixed.txt', decryptsText);
+    console.log(decryptsText);
+  } catch {
+    let zero: number = 0;
+    return console.log(zero);
+  }
+}
+fileName('reversed-order.txt');
