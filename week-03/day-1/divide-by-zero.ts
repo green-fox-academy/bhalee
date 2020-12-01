34e2cb7e3f89ec9b8176a95507370d72f98c3e22
Balazs Kover <koverbalazs@gmail.com>
2020-12-01 12:04:07 +0100

    divide-by-zero.ts done

---
+++ week-03/day-1/divide-by-zero.ts
@@ -0,0 +1,16 @@
+'use strict';
+  
+// Create a function that takes a number
+// divides ten with it,
+// and prints the result.
+// It should print 'fail' if the parameter is 0
+
+function divideNumber(x:number){
+  let sum:number|string = x / 10;
+  if (sum === 0){
+    sum = 'fail'
+  }
+  return sum;
+}
+console.log(divideNumber(0));
+
