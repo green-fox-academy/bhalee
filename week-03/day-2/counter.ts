010ad6b3e2999fb3b2d2b83516630dc93a0c2425
Balazs Kover <koverbalazs@gmail.com>
2020-12-02 15:51:47 +0100

    counter.ts done

---
+++ week-03/day-2/counter.ts
@@ -0,0 +1,43 @@
+'use strict';
+
+export class Counter {
+  private _number: number;
+  private _startNumber: number;
+
+  constructor(number?: number) {
+    if (number === undefined) {
+      this._number = 0;
+      this._startNumber = 0;
+    } else {
+      this._number = number;
+      this._startNumber = number;
+    }
+  }
+
+  public add(number?: number) {
+    if (number) {
+      this._number += number;
+    } else {
+      this._number++;
+    }
+  }
+
+  public get() {
+    let stringNumber: string | number = this._number;
+    return stringNumber;
+  }
+
+  public reset() {
+    this._number = this._startNumber;
+  }
+}
+
+// let szam = new Counter(100);
+// console.log(szam);
+// szam.add(5);
+// szam.get;
+// console.log(szam.get())
+// szam.reset();
+// szam.add();
+// console.log(szam.get())
+
---
+++ week-03/day-2/sharpie.ts
@@ -0,0 +1,17 @@
+class Sharpie{
+  private _color:string;
+  private _width:number;
+  private _inkAmount: number;
+
+  constructor(color:string, width:number){
+    this._color = color;
+    this._width = width;
+    this._inkAmount = 100;
+  }
+  public use(){
+    this._inkAmount--;
+  }
+}
+let sword = new Sharpie('silver', 100)
+sword.use()
+console.log(sword);
