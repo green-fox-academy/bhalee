f3ed562e70c7df878f5692decf25d8e0c790a68a
Balazs Kover <koverbalazs@gmail.com>
2020-12-02 17:38:25 +0100

    fleetOfThings.ts started

---
+++ week-03/day-2/fleetOfThings.ts
@@ -0,0 +1,30 @@
+import { Thing } from './thing';
+import { Fleet } from './fleet';
+
+let fleet = new Fleet();
+
+// -  You have the `Thing` class
+// -  You have the `Fleet` class
+// -  Download those, use those
+// -  Achieve this output:
+//  Crete a fleet of things to have this output:
+
+// 1. [ ] Get milk
+// 2. [ ] Remove the obstacles
+// 3. [x] Stand up
+// 4. [x] Eat lunch
+
+let milk = new Thing('Get milk');
+let remove = new Thing('Remove the obstacles')
+let stand = new Thing('Stand up')
+let eat = new Thing('Eat lunch')
+
+fleet.add(milk)
+fleet.add(remove)
+fleet.add(stand)
+fleet.add(eat)
+
+
+
+
+console.log(fleet.toString());
