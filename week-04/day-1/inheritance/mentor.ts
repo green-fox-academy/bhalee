0f7e6b0658eebb556988a265e61d301c0a87bfaf
Balazs Kover <koverbalazs@gmail.com>
2020-12-07 15:15:05 +0100

    sponsor.ts done

---
+++ week-04/day-1/inheritance/mentor.ts
@@ -0,0 +1,17 @@
+import{Person} from './person'
+class Mentor extends Person{
+
+protected _level:string;
+constructor (name?:string, age?:number, gender?:string, level?:string){
+super(name,age,gender)
+this._level = level ?? 'intermediate';
+}
+
+getGoal():void{
+  console.log(`My goal is: Educate brilliant junior software developers`);
+}
+
+introduce():void{
+  console.log(`Hi, I'm ${this._name}, a ${this._age} year old ${this._gender} ${this._level} mentor.`);
+}
+}
---
+++ week-04/day-1/inheritance/sponsor.ts
@@ -0,0 +1,26 @@
+import { Person } from './person';
+class Sponsor extends Person {
+  protected _company: string;
+  protected _hiredStudents: number;
+
+  constructor (name?:string, age?:number, gender?:string, company?:string){
+    super(name,age,gender)
+    this._company = company ?? 'Google';
+    this._hiredStudents = 0;
+  
+  }
+
+  introduce():void {
+    console.log(`Hi, I'm ${this._name}, a ${this._age} year old ${this._gender} who represents ${this._company} and hired ${this._hiredStudents} students so far.'`);
+    
+  }
+
+  hire():void {
+    this._hiredStudents++
+    
+  }
+  getGoal():void{
+    console.log(`My goal is: Hire brilliant junior software developers.`);
+    
+  }
+}
