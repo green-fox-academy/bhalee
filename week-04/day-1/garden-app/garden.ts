ba6242ceb7daf3aca8a827223447f2ad046f9a12
Balazs Kover <koverbalazs@gmail.com>
2020-12-07 21:51:57 +0100

    garden done.plants.ts instead of garden.ts and flowers.ts

--- week-04/day-1/garden-app/garden.ts
+++ week-04/day-1/garden-app/garden.ts
@@ -9,38 +9,46 @@ class Garden {
   }
 
   addPlant(plantName: string, plantType): void {
-    this._gardenPlants.push(...[new Plants(plantName,plantType)]);
+    this._gardenPlants.push(...[new Plants(plantName, plantType)]);
   }
 
   info() {
     for (let i: number = 0; i < this._gardenPlants.length; i++) {
-      console.log(this._gardenPlants[i].infoName());
-      console.log(this._gardenPlants[i].infoWaterAmount());
+      if (this._gardenPlants[i].infoWaterAmount() < 5) {
+        console.log(`The ${this._gardenPlants[i].infoName()} ${this._gardenPlants[i].infoType()} needs water`);
+      } else {
+        console.log(`The ${this._gardenPlants[i].infoName()} ${this._gardenPlants[i].infoType()} doesnt need water`);
+      }
     }
   }
   watering(waterAmount: number) {
+    console.log(`Watering with ${waterAmount}`);
     let needWater: number = 0;
-
     for (let i: number = 0; i < this._gardenPlants.length; i++) {
-
       if (this._gardenPlants[i].infoWaterAmount() < 5) {
         needWater++;
       }
     }
 
     for (let i: number = 0; i < this._gardenPlants.length; i++) {
-      if (this._gardenPlants[i].infoWaterAmount() < 5|| this._gardenPlants[i].infoType() === 'tree') {
-        this._gardenPlants[i].addwater((waterAmount / needWater)*0.40 );
+      if (this._gardenPlants[i].infoWaterAmount() < 5 && this._gardenPlants[i].infoType() === 'Tree') {
+         this._gardenPlants[i].addwater((waterAmount / needWater) * 0.4);
       }
-      if (this._gardenPlants[i].infoWaterAmount() < 5|| this._gardenPlants[i].infoType() === 'flower') {
-        this._gardenPlants[i].addwater((waterAmount / needWater)*0.75 );
+
+      if (this._gardenPlants[i].infoWaterAmount() < 5 && this._gardenPlants[i].infoType() === 'Flower') {
+        this._gardenPlants[i].addwater((waterAmount / needWater) * 0.75);
       }
     }
   }
 }
-
-let kiskert = new Garden('kiskert');
-kiskert.addPlant('yellow','tree');
-kiskert.addPlant('blue','flower');
-kiskert.watering(40);
-kiskert.info();
+//Test
+let myLittleGarden = new Garden('Little Garden');
+myLittleGarden.addPlant('yellow', 'Flower');
+myLittleGarden.addPlant('blue', 'Flower');
+myLittleGarden.addPlant('orange', 'Tree');
+myLittleGarden.addPlant('purple', 'Tree');
+myLittleGarden.info();
+myLittleGarden.watering(40);
+myLittleGarden.info();
+myLittleGarden.watering(70);
+myLittleGarden.info();
