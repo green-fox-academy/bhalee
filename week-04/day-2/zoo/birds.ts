2d5f4f08014ee543dc1cdee8beba1d297d78c63e
Balazs Kover <koverbalazs@gmail.com>
2020-12-08 16:43:39 +0100

    Flyable implement to birds.ts in zoo exercise

--- week-04/day-2/zoo/birds.ts
+++ week-04/day-2/zoo/birds.ts
@@ -1,7 +1,8 @@
 'use strict';
 import { EggLaying } from './egglaying';
+import {Flyable} from '../flyable/flyable'
 
-export class Bird extends EggLaying {
+export class Bird extends EggLaying implements Flyable {
   constructor(name: string, age?: number, borned?: string, petable?: boolean) {
     super(name, age, borned, petable);
     this._name = name;
@@ -9,10 +10,20 @@ export class Bird extends EggLaying {
     this._borned = borned;
     this._petAble = petable;
   }
+  land(): string {
+    return 'I can land quick and precisely';
+  }
+  fly(): string {
+    return 'I can fly like a hawk';
+  }
+  takeoff(): string {
+    return 'I can takeoff quick and precisely';
+  }
   getName(): string {
     return this._name;
   }
   eat(): string {
     return 'I like eat seeds';
   }
+  
 }
