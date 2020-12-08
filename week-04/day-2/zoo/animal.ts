4b71b3ee840bf6dd4b48ce9f404fde92e4c2a98c
Balazs Kover <koverbalazs@gmail.com>
2020-12-08 16:16:53 +0100

    vars changed to protected and birds.ts done

--- week-04/day-2/zoo/animal.ts
+++ week-04/day-2/zoo/animal.ts
@@ -1,5 +1,12 @@
 'use strict';
 export abstract class Animal {
+  constructor(name, age, borned, petable){
+   this._name = name;
+   this._age = age;
+   this._borned = borned;
+   this._petAble = petable;
+
+  }
   protected _name: string;
   protected _age: number;
   protected _borned: string;
--- week-04/day-2/zoo/birds.ts
+++ week-04/day-2/zoo/birds.ts
@@ -1,1 +1,19 @@
 'use strict';
+import {EggLaying} from './egglaying'
+
+export class Bird extends EggLaying{
+  constructor(name:string,age?:number,borned?:string, petable?:boolean){
+    super(name,age, borned,petable)
+    this._name = name;
+    this._age = age;
+    this._borned = borned;
+    this._petAble = petable;
+    
+  }
+  getName(): string {
+    return this._name;
+  }
+  eat(): string {
+    return 'I like eat seeds';
+  }
+}
