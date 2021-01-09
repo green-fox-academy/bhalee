c8ebdb2c7ca9d2617a9a42477f8c4cfe0ba46428
Balazs Kover <koverbalazs@gmail.com>
2021-01-09 18:07:37 +0100

    arrays changed to objects

--- week-06/day-5/image-gallery/gallery.js
+++ week-06/day-5/image-gallery/gallery.js
@@ -1,33 +1,63 @@
 'use strict';
-let imageList = ['/img/random1.jpg', '/img/random2.jpg', '/img/random3.jpg', '/img/random4.jpg', '/img/random5.jpg', '/img/random6.jpg', '/img/random7.jpg', '/img/random8.jpg'];
-let imageTitle = ['The field', 'Sunset', 'The lake', 'Dream river', 'Montains', 'Snow white', 'Sloth life', 'Welcome to Africa'];
-let imageDest = [
-  `Field ipsum, dolor sit amet consectetur adipisicing elit. Voluptate modi deleniti iste laboriosam quasi dolores magnam ipsum eligendi obcaecati blanditiis repellendus id saepe tempore, asperiores eaque maxime exercitationem ducimus nisi.`,
-  `Sunset ipsum, dolor sit amet consectetur adipisicing elit. Voluptate modi deleniti iste laboriosam quasi dolores magnam ipsum eligendi obcaecati blanditiis repellendus id saepe tempore, asperiores eaque maxime exercitationem ducimus nisi.`,
-  `Lake ipsum, dolor sit amet consectetur adipisicing elit. Voluptate modi deleniti iste laboriosam quasi dolores magnam ipsum eligendi obcaecati blanditiis repellendus id saepe tempore, asperiores eaque maxime exercitationem ducimus nisi.`,
-  `River ipsum, dolor sit amet consectetur adipisicing elit. Voluptate modi deleniti iste laboriosam quasi dolores magnam ipsum eligendi obcaecati blanditiis repellendus id saepe tempore, asperiores eaque maxime exercitationem ducimus nisi.`,
-  `Montain ipsum, dolor sit amet consectetur adipisicing elit. Voluptate modi deleniti iste laboriosam quasi dolores magnam ipsum eligendi obcaecati blanditiis repellendus id saepe tempore, asperiores eaque maxime exercitationem ducimus nisi.`,
-  `Snow ipsum, dolor sit amet consectetur adipisicing elit. Voluptate modi deleniti iste laboriosam quasi dolores magnam ipsum eligendi obcaecati blanditiis repellendus id saepe tempore, asperiores eaque maxime exercitationem ducimus nisi.`,
-  `Sloth ipsum, dolor sit amet consectetur adipisicing elit. Voluptate modi deleniti iste laboriosam quasi dolores magnam ipsum eligendi obcaecati blanditiis repellendus id saepe tempore, asperiores eaque maxime exercitationem ducimus nisi.`,
-  `Africa ipsum, dolor sit amet consectetur adipisicing elit. Voluptate modi deleniti iste laboriosam quasi dolores magnam ipsum eligendi obcaecati blanditiis repellendus id saepe tempore, asperiores eaque maxime exercitationem ducimus nisi.`,
-];
+let imageList = [
+  {
+    url: '/img/random1.jpg',
+    title: 'The field',
+    dest: `Field ipsum, dolor sit amet consectetur adipisicing elit. Voluptate modi deleniti iste laboriosam quasi dolores magnam ipsum eligendi obcaecati blanditiis repellendus id saepe tempore, asperiores eaque maxime exercitationem ducimus nisi.`,
+  },
+  {
+    url: '/img/random2.jpg',
+    title: 'Sunset',
+    dest: `Sunset ipsum, dolor sit amet consectetur adipisicing elit. Voluptate modi deleniti iste laboriosam quasi dolores magnam ipsum eligendi obcaecati blanditiis repellendus id saepe tempore, asperiores eaque maxime exercitationem ducimus nisi.`,
+  },
+  {
+    url: '/img/random3.jpg',
+    title: 'The lake',
+    dest: `Lake ipsum, dolor sit amet consectetur adipisicing elit. Voluptate modi deleniti iste laboriosam quasi dolores magnam ipsum eligendi obcaecati blanditiis repellendus id saepe tempore, asperiores eaque maxime exercitationem ducimus nisi.`,
+  },
+  {
+    url: '/img/random4.jpg',
+    title: 'Dream river',
+    dest: `River ipsum, dolor sit amet consectetur adipisicing elit. Voluptate modi deleniti iste laboriosam quasi dolores magnam ipsum eligendi obcaecati blanditiis repellendus id saepe tempore, asperiores eaque maxime exercitationem ducimus nisi.`,
+  },
+  {
+    url: '/img/random5.jpg',
+    title: 'Montains',
+    dest: `Montain ipsum, dolor sit amet consectetur adipisicing elit. Voluptate modi deleniti iste laboriosam quasi dolores magnam ipsum eligendi obcaecati blanditiis repellendus id saepe tempore, asperiores eaque maxime exercitationem ducimus nisi.`,
+  },
+  {
+    url: '/img/random6.jpg',
+    title: 'Snow white',
+    dest: `Snow ipsum, dolor sit amet consectetur adipisicing elit. Voluptate modi deleniti iste laboriosam quasi dolores magnam ipsum eligendi obcaecati blanditiis repellendus id saepe tempore, asperiores eaque maxime exercitationem ducimus nisi.`,
+  },
+  {
+    url: '/img/random7.jpg',
+    title: 'Sloth life',
+    dest: `Sloth ipsum, dolor sit amet consectetur adipisicing elit. Voluptate modi deleniti iste laboriosam quasi dolores magnam ipsum eligendi obcaecati blanditiis repellendus id saepe tempore, asperiores eaque maxime exercitationem ducimus nisi.`,
+  },
+  {
+    url: '/img/random8.jpg',
+    title: 'Welcome to Africa',
+    dest: `Africa ipsum, dolor sit amet consectetur adipisicing elit. Voluptate modi deleniti iste laboriosam quasi dolores magnam ipsum eligendi obcaecati blanditiis repellendus id saepe tempore, asperiores eaque maxime exercitationem ducimus nisi.`,
+  },
+]
 
 let counter = 0;
 const imageShow = document.querySelector('#image-show');
 const theImage = document.createElement('img');
 theImage.setAttribute('id', 'theImage');
-theImage.setAttribute('src', imageList[0]);
+theImage.setAttribute('src', imageList[0].url);
 imageShow.appendChild(theImage);
 
 const imageDescription = document.querySelector('#image-description');
 const title = document.createElement('h1');
 title.setAttribute('class', 'imageTitle');
-title.innerHTML = imageTitle[0];
+title.innerHTML = imageList[0].title;
 imageDescription.appendChild(title);
 
 const description = document.createElement('p');
 description.setAttribute('class', 'imageDescription');
-description.innerHTML = imageDest[0];
+description.innerHTML = imageList[0].dest;
 imageDescription.appendChild(description);
 
 for (let i = 0; i < imageList.length; i++) {
@@ -38,7 +68,7 @@ for (let i = 0; i < imageList.length; i++) {
 
   const imgIcons = document.createElement('img');
   imgIcons.setAttribute('id', 'theIcons');
-  imgIcons.setAttribute('src', imageList[i]);
+  imgIcons.setAttribute('src', imageList[i].url);
   if (i === 0) {
     imgIcons.setAttribute('class', 'selected');
   } else {
@@ -51,24 +81,21 @@ for (let i = 0; i < imageList.length; i++) {
 const theIcons = document.querySelectorAll('#theIcons');
 const deleteTitle = document.querySelector('.imageTitle');
 const deleteDest = document.querySelector('.imageDescription');
-
 for (let i = 0; i < theIcons.length; i++) {
     theIcons[i].onclick = () => {
     deleteTitle.remove();
-    
-    title.innerHTML = imageTitle[i];
+    title.innerHTML = imageList[i].title;
     imageDescription.appendChild(title);
 
     deleteDest.remove();
-    
-    description.innerHTML = imageDest[i];
+    description.innerHTML = imageList[i].dest;
     imageDescription.appendChild(description);
 
     theIcons[i].setAttribute('class', 'selected');
     theIcons[counter].setAttribute('class', 'notSelected');
     const toShow = theIcons[i].getAttribute('src');
     for (let j = 0; j < imageList.length; j++) {
-      if (toShow === imageList[i]) {
+      if (toShow === imageList[i].url) {
         counter = i;
       }
     }
@@ -89,14 +116,14 @@ rightButton.onclick = () => {
     counter += 1;
   }
 
-  title.innerHTML = imageTitle[counter];
+  title.innerHTML = imageList[counter].title;
   imageDescription.appendChild(title);
-
-  description.innerHTML = imageDest[counter];
+  
+  description.innerHTML = imageList[counter].dest;
   imageDescription.appendChild(description);
 
   theIcons[counter].setAttribute('class', 'selected');
-  theImage.setAttribute('src', imageList[counter]);
+  theImage.setAttribute('src', imageList[counter].url);
   imageShow.appendChild(theImage);
 };
 
@@ -113,14 +140,14 @@ leftButton.onclick = () => {
     counter -= 1;
   }
 
-  title.innerHTML = imageTitle[counter];
+  title.innerHTML = imageList[counter].title;
   imageDescription.appendChild(title);
 
-  description.innerHTML = imageDest[counter];
+  description.innerHTML = imageList[counter].dest;
   imageDescription.appendChild(description);
 
   theIcons[counter].setAttribute('class', 'selected');
 
-  theImage.setAttribute('src', imageList[counter]);
+  theImage.setAttribute('src', imageList[counter].url);
   imageShow.appendChild(theImage);
 };
