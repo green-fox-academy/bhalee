f42c300696e7e10de845d71897ee61a4130f09b6
Balazs Kover <koverbalazs@gmail.com>
2021-01-13 21:20:45 +0100

    star wars API forced and done

--- week-07/day-3/starwars/app.js
+++ week-07/day-3/starwars/app.js
@@ -1,111 +1,79 @@
-// const xhr = new XMLHttpRequest();
-
-// xhr.onload = () => {
-//   let list = JSON.parse(xhr.response);
-//   console.log(list.results);
-//   for (let i = 0; i < list.results.length; i++) {
-//     console.log(list.results[i]);
-//   }
-// };
-// let method = 'GET';
-// let endpoint = 'http://swapi.dev/api/people/';
-// xhr.open(method, endpoint);
-
-// xhr.send();
-
-function addToList(name) {
+'use strict';
+function addToList(name, id) {
   let list = document.querySelector('#list');
   let listElem = document.createElement('li');
+
   listElem.setAttribute('class', 'resultName');
-  listElem.setAttribute('id', 'b1');
+  listElem.setAttribute('id', id);
+  listElem.style.cursor = 'pointer';
   listElem.innerHTML = `${name}`;
+
   list.appendChild(listElem);
 }
+
 function clearList() {
   let list = document.querySelector('#list');
   list.innerHTML = '';
 }
 
-function addDest(id,title,dest) {
-  const character = document.querySelector(`#Luke`);
-  console.log(character);
-  let forTitle = document.createElement('p');
-  let fordest = document.createElement('p');
-  forTitle.innerHTML = title.toString()
-  fordest.innerHTML = dest.toString()
-  character.appendChild(forTitle)
-  character.appendChild(fordest)
-}
-
 const button = document.querySelector('#add-expense');
 const inputName = document.querySelector('#add-name');
+
 button.onclick = () => {
+
   const xhr = new XMLHttpRequest();
   xhr.onload = () => {
     let list = JSON.parse(xhr.response);
-    console.log(list.results);
-    for (let i = 0; i < list.results.length; i++) {
-      // console.log(list.results[i]);
-    }
-
     clearList();
-    let searchName = inputName.value.toLowerCase();
 
+    let counter = 0;
+    let searchName = inputName.value.toLowerCase();
     for (let i = 0; i < list.results.length; i++) {
       let searchElem = list.results[i].name.toLowerCase();
       let splitElem = searchElem.split(' ');
+
       for (let j = 0; j < splitElem.length; j++) {
         if (searchName === searchElem || splitElem[j] === searchName) {
-          addToList(list.results[i].name);
+          let id = `id${counter}`;
+          addToList(list.results[i].name, id);
+          counter++;
         }
       }
     }
+
     const listedElem = document.querySelectorAll('.resultName');
     for (let i = 0; i < listedElem.length; i++) {
       listedElem[i].onclick = () => {
-        let searchName = listedElem[i].innerHTML;
 
+        let searchName = listedElem[i].innerHTML;
         const xhr2 = new XMLHttpRequest();
+
         xhr2.onload = () => {
           let list2 = JSON.parse(xhr2.response);
           for (let t = 0; t < list2.results.length; t++) {
+
             if (searchName === list2.results[t].name) {
-              
+
               for (let index = 0; index < list2.results[t].films.length; index++) {
-                
-                
                 const xhr = new XMLHttpRequest();
-                
-                const character = document.querySelector('#b1');
                 xhr.onload = () => {
-                  
+                  const character = document.querySelector(`#id${i}`);
                   let filmList = JSON.parse(xhr.response);
-                  // console.log(filmList.title);
-                  // console.log(filmList.release_date);
-                  // console.log(searchName);
-                  // addDest(searchName, filmList.title,filmList.release_date)
-                  // console.log(character);
                   let forTitle = document.createElement('p');
-                  let fordest = document.createElement('p');
-                  forTitle.innerHTML = filmList.title
-                  fordest.innerHTML = filmList.release_date
-                  character.appendChild(forTitle)
-                  character.appendChild(fordest)
-
-
-                }
+                  let fordate = document.createElement('p');
+                  forTitle.style.cursor = 'text';
+                  fordate.style.cursor = 'text';
+                  forTitle.innerHTML = filmList.title;
+                  fordate.innerHTML = filmList.release_date;
+                  character.appendChild(forTitle);
+                  character.appendChild(fordate);
+                };
                 let method = 'GET';
                 let endpoint = list2.results[t].films[index];
                 xhr.open(method, endpoint);
-                
                 xhr.send();
-              };
-
+              }
             }
-          
-
-
-            
           }
         };
         let method2 = 'GET';
