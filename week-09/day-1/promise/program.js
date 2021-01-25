96d55f3a5fbe0c5b1049c833bc37214191c1e245
bhalee <koverbalazs@gmail.com>
2021-01-25 14:28:24 +0100

    first ready

---
+++ week-09/day-1/promise/program.js
@@ -0,0 +1,26 @@
+'use strict'
+// function timer(callback){
+// setTimeout( ()=>{
+// callback()
+// },300)
+// }
+
+// function log(){
+//   console.log('TIMED OUT!');
+// }
+
+// timer(log)
+
+function promiseWith() {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      setTimeout (()=> resolve('TIMED OUT!'),300)
+    })
+  })
+}
+
+promiseWith()
+.then(response => { console.log(response);})
+.catch(function (err){
+  console.log(err);
+})
